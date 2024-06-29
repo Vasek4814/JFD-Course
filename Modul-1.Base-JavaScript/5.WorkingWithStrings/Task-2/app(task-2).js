044e3e850a6c8f9db2474615780d331251e523b7
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-06-29 16:07:38 +0300

    fix: const/let and chain methods

--- Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-2/app(task-2).js
+++ Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-2/app(task-2).js
@@ -5,7 +5,7 @@ const courseCreatorName = 'Владилена Минина';
 const reasonText = 'это круто, интересно, и хороший доход';
 const numberOfMonth = '6';
 
-let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) programmingLanguage ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`;
+const myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) programmingLanguage ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`;
 console.log(myInfoText);
 console.log(
   myInfoText.replaceAll(programmingLanguage, programmingLanguage.toLowerCase())
