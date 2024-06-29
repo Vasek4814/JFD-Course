556f7153e6049d5dc1837aa877e877d5d5eec210
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-06-29 15:47:08 +0300

    fix: const/let

--- Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-4/app(task-4).js
+++ Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-4/app(task-4).js
@@ -2,14 +2,6 @@ const userName = prompt('Как вас зовут?').trim().toLowerCase();
 console.log(userName);
 const userAge = Number(prompt('Сколько вам лет?').trim());
 console.log(userAge);
-// alert(`Вас зовут ${userName} и вам ${userAge}`)
-// Не переобразуеться из string в number, хотя в консоле, подсвет синий
-
-// if (typeof userAge === 'number') {
-//     alert(`Вас зовут ${userName} и вам ${userAge}`)
-// } else {
-//     alert('Досвидания!!!')
-// }
 if (Number.isNaN(userAge)) {
   alert('Досвидания!!!');
 } else {
--- Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-5/app(task-5).js
+++ Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-5/app(task-5).js
@@ -1,13 +1,13 @@
-const userString = prompt('Введите текст для обрезки');
-console.log(userString.trim());
-const startSliceIndex = +prompt(
+let userString = prompt('Введите текст для обрезки');
+userString = userString.trim();
+let startSliceIndex = +prompt(
   'Введите индекс, с которого нужно начать обрезку строки'
 );
 console.log(startSliceIndex);
-const endSliceIndex = +prompt(
+let endSliceIndex = +prompt(
   'Введите индекс, которым нужно закончить обрезку строки'
 );
 console.log(endSliceIndex);
-const result = userString.slice(startSliceIndex, endSliceIndex);
+let result = userString.slice(startSliceIndex, endSliceIndex);
 console.log(result);
 alert(result);
