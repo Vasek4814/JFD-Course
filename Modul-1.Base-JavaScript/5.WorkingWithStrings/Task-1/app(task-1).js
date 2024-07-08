f6fa3b5ec62e0ce13d512414d97c570429bad408
Vasek4814 <150356629+Vasek4814@users.noreply.github.com>
2024-07-09 00:48:28 +0300

    Delete Modul-1.Base-JavaScript/5.WorkingWithStrings directory

--- Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-1/app(task-1).js
+++
@@ -1,8 +0,0 @@
-const myName = 'Вася';
-const programmingLanguage = 'JavaScript';
-const courseCreatorName = 'Владилена Минина';
-const reasonText = 'это круто, интересно, и хороший доход';
-const numberOfMonth = '6';
-
-const myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) programmingLanguage ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`;
-console.log(myInfoText);
--- Modul-1.Base-JavaScript/5.WorkingWithStrings/Task-4/app(task-4).js
+++
@@ -1,9 +0,0 @@
-const userName = prompt('Как вас зовут?').trim().toLowerCase();
-console.log(userName);
-const userAge = Number(prompt('Сколько вам лет?').trim());
-console.log(userAge);
-if (Number.isNaN(userAge)) {
-  alert('Досвидания!!!');
-} else {
-  alert(`Вас зовут ${userName} и вам ${userAge}`);
-}
