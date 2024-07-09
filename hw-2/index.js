1d58854a48e34670950e661eb96b5ae8f38d8ef7
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-07-09 16:57:26 +0300

    fix: /

--- hw-2/index.js
+++ hw-2/index.js
@@ -1,12 +1,11 @@
-let javaScriptDescription =
+const javaScriptDescription =
   'JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript. JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений';
-let finalString = Math.floor(javaScriptDescription.length / 2);
-finalString = Number(finalString);
+const index = Math.floor(javaScriptDescription.length / 2);
 javaScriptDescription = javaScriptDescription
-  .slice(0, finalString)
+  .slice(0, index)
   .replaceAll('a', 'A')
   .replaceAll('а', 'А')
   .replaceAll(' ', '')
   .repeat(3);
-console.log(javaScriptDescription.charAt(finalString));
+console.log(javaScriptDescription.charAt(index));
 console.log(javaScriptDescription);
