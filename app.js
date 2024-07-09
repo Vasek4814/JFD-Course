3ef2bbf866733cf9d8974a900188956347e9b05f
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-07-09 16:34:51 +0300

    fix: corrected the condition

--- app.js
+++
@@ -1,5 +0,0 @@
-console.log('Таймер запускается.')
-for (let i = 0; i <= 5: i++) {
-    console.log(i)
-}
-console.log('Таймер окончен.')
--- hw-2/index.js
+++ hw-2/index.js
@@ -1,15 +1,12 @@
 let javaScriptDescription =
   'JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили. Является реализацией спецификации ECMAScript. JavaScript обычно используется как встраиваемый язык для программного доступа к объектам приложений';
-let sliceString = javaScriptDescription.length / 2;
-(sliceString = Math.floor(sliceString)), Number(sliceString);
+let finalString = Math.floor(javaScriptDescription.length / 2);
+finalString = Number(finalString);
 javaScriptDescription = javaScriptDescription
-  .slice(0, sliceString)
+  .slice(0, finalString)
   .replaceAll('a', 'A')
   .replaceAll('а', 'А')
   .replaceAll(' ', '')
   .repeat(3);
-console.log(
-  javaScriptDescription,
-  javaScriptDescription.length / 2,
-  Math.floor(javaScriptDescription.length / 2)
-);
+console.log(javaScriptDescription.charAt(finalString));
+console.log(javaScriptDescription);
