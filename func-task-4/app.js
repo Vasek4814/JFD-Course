86d13043699c360a465bc473f18b60851691d63b
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-08-13 17:52:25 +0300

    done

--- func-task-4/app.js
+++ func-task-4/app.js
@@ -1,6 +1,6 @@
 const checkQuestionAnswer = (question, correctAnswer) => {
   let questionUser = prompt(question);
-  let answerUser = prompt(correctAnswer);
+  let answerUser = correctAnswer;
   if (
     questionUser.trim().toLocaleLowerCase() === answerUser.toLocaleLowerCase()
   ) {
@@ -9,4 +9,6 @@ const checkQuestionAnswer = (question, correctAnswer) => {
     alert('Ответ не верный');
   }
 };
-checkQuestionAnswer('Арбуз это фрукт или ягода?', 'Ягода');
+// checkQuestionAnswer('Арбуз это фрукт или ягода?', 'Ягода');
+// checkQuestionAnswer('Сколько в среднем зубов у взрослого человека?', '32');
+checkQuestionAnswer('Как называется самая маленькая птица в мире?', 'Колибри');
