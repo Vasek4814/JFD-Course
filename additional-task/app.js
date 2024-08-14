3e65a65f3d692a306e5f8803e2c96325c98b2a72
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-08-14 22:38:22 +0300

    fixed errors

---
+++ additional-task/app.js
@@ -0,0 +1,11 @@
+const showSuccessMessage = (message) => {
+  console.log(message);
+  return;
+};
+
+const showErrorMessage = (message) => {
+  console.error(message);
+  return;
+};
+
+showErrorMessage(54);
