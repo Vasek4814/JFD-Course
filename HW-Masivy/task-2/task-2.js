dd0f51b4ca7dfa77879d0e4f0786b0158a535848
Vasiliy <89585704814v@gmail.com>
2024-09-19 10:45:22 +0300

    add new files htmla and js

---
+++ HW-Masivy/task-2/task-2.js
@@ -0,0 +1,24 @@
+const peopleWaiting = ['Кристина', 'Олег', 'Кирилл', 'Мария',
+ 'Cветлана', 'Артем', 'Глеб']
+ console.log(peopleWaiting)
+
+
+
+
+
+ const giveParcel = (name, delet) => {
+    alert(`${name} получил(а) посылку. 
+    В очереди осталось ${length} человек`)
+    return
+ }
+
+ giveParcel(peopleWaiting[0], peopleWaiting.splice())
+
+
+
+
+ const leaveQueueWithoutParcel = (name) => {
+
+ }
+ 
+
