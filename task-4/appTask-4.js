0b4f034d69fd15dad0131d3bdd0b4e599dfbb2f4
Vasek4814 <150356629+Vasek4814@users.noreply.github.com>
2024-09-23 12:42:37 +0300

    Delete task-4 directory

--- task-4/appTask-4.js
+++
@@ -1,8 +0,0 @@
-const checkQuestionAnswer = (question, correctAnswer) => {
-  const questionUser = prompt(question);
-  if (questionUser.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-    alert('Ответ верный');
-  } else {
-    alert('Ответ не верный');
-  }
-};
