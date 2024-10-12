d0b912e1af4ca6902019eb938dac013346c75ba7
Vasiliy Naberezhniy <89585704814v@gmail.com>
2024-10-12 14:56:41 +0300

    task-4 done

--- HW-Object/task-4/app.js
+++ HW-Object/task-4/app.js
@@ -1,23 +1,29 @@
 const getKiller = (suspectinfo, deadpeople) => {
+  for (const name in suspectinfo) {
+    const peoples = suspectinfo[name];
+    const isKiller = deadpeople.every((name) => peoples.includes(name));
+    if (isKiller) {
+      return name;
+    }
+    console.log(name, isKiller);
+  }
+};
 
+const result = getKiller(
+  {
+    James: ['Jacob', 'Bill', 'Lucas'],
+    Johnny: ['David', 'Kyle', 'Lucas'],
+    Peter: ['Lucy', 'Kyle'],
+  },
+  ['Lucas', 'Bill']
+);
 
-
-    
-}
-
-getKiller(
-    {
-        'James': ['Jacob', 'Bill', 'Lucas'],
-        'Johnny': ['David', 'Kyle', 'Lucas'],
-        'Peter': ['Lucy', 'Kyle'],
-    }, ['Lucas', 'Bill']
- );
-  
- getKiller(
-    {
-        'Brad': [],
-        'Megan': ['Ben', 'Kevin'],
-        'Finn': [],
-    },
-    ['Ben']
- );
+const result2 = getKiller(
+  {
+    Brad: [],
+    Megan: ['Ben', 'Kevin'],
+    Finn: [],
+  },
+  ['Ben']
+);
+console.log(result, result2);
