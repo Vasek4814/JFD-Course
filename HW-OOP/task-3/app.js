759e33c7a263f1df28f74a74930e7a127058839f
Vasiliy Naberezhniy <89585704814v@gmail.com>
2025-03-03 15:19:53 +0300

    task-3 done

---
+++ HW-OOP/task-3/app.js
@@ -0,0 +1,29 @@
+class CarService {
+  static DefaultWorkingHours = {
+    from: '9:00',
+    till: '20:00',
+  };
+  constructor(name, workingHours) {
+    this.name = name;
+    this.workingHours = workingHours || CarService.DefaultWorkingHours;
+  }
+  repairCar(carName) {
+    const from = Number(this.workingHours.from.split(':')[0]);
+    const till = Number(this.workingHours.till.split(':')[0]);
+    const currentHours = new Date().getHours();
+    if (!carName) {
+      console.error(
+        'Вам необходимо указать название машины, чтоб её отремонтровали'
+      );
+    } else if (currentHours > from && currentHours < till) {
+      console.log(
+        `Сейчас отремонтируем вашу машину ${carName}! Ожидайте, пожалуйста!`
+      );
+    } else console.log('К сожалению, мы сейчас закрыты. Приходите завтра');
+  }
+}
+const carService = new CarService('RepairCarNow', {
+  from: '8:00',
+  till: '20:00',
+});
+carService.repairCar('BMW');
